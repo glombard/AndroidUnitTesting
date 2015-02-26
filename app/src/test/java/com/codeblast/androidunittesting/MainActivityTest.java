eecd2b11691c015ec5d66f5feeb94f2b8bfc1b66
Gert Lombard <gert.lombard@gmail.com>
2015-02-25 21:05:21 -0800

    add a silly unit test to see if jacoco works

---
+++ app/src/test/java/com/codeblast/androidunittesting/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.codeblast.androidunittesting;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+  @Test
+  public void testNothing() {
+    MainActivity a = new MainActivity();
+  }
+}
